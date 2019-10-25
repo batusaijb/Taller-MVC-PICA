019798966fcf29796670081451411b4607fc08be
batusaijb <42442877+batusaijb@users.noreply.github.com>
2019-10-25 14:15:54 -0500

    Add files via upload

---
+++ Models/UserProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taller1.Models
+{
+    public class UserProfile
+    {
+        public int userId { get; set; }
+
+        [Required]
+        public string userName { get; set; }
+
+        [Required]
+        public string password { get; set; }
+
+    }
+}
